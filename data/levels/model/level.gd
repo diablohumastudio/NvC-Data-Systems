8d272b7a6c214814016eda6f1925dddc66a25bb8
diablohumastudioadmin <admin@diablohumastudio.com>
2025-06-26 17:34:46 -0500

    other files in more levels created two commits before

--- data/levels/model/level.gd
+++ data/levels/model/level.gd
@@ -1,6 +1,6 @@
 class_name Level extends Resource
 
-enum IDs {Level1, Level2}
+enum IDs {Level1, Level2, Level2p, Level3, Level4}
 
 @export var id: IDs
 @export var level_name: String
@@ -8,6 +8,6 @@ enum IDs {Level1, Level2}
 
 func _set_ud_level(new_value:UDLevel) -> void:
 	ud_level = new_value
-	for saved_ud_lv in UserDataSystem.user_data.progress.ud_levels:
+	for saved_ud_lv in UserDataSystem.current_user_data.progress.ud_levels:
 		if saved_ud_lv.id == ud_level.id:
 			ud_level = saved_ud_lv
