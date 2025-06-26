163c47a87fb270a5d447f53f5e9b96369acd5d70
diablohumastudioadmin <admin@diablohumastudio.com>
2025-06-26 17:29:41 -0500

    create ud_levels from files from user_progress to user_data_system

--- global_systems/user_data_system/user_data_system.gd
+++ global_systems/user_data_system/user_data_system.gd
@@ -11,7 +11,19 @@ func load_user_data():
 	if ResourceLoader.exists(_FILE_NAME):
 		user_data = load(_FILE_NAME)
 	else:
-		user_data = UserData.new()
+	_user_data.progress.ud_levels = _create_ud_levels_from_res_files()
+func _create_ud_levels_from_res_files() -> Array[UDLevel]:
+	var _ud_levels: Array[UDLevel]
+	var dir := DirAccess.open("res://data/ud_levels/data/")
+	assert(dir != null, "Could not open folder")
+	dir.list_dir_begin()
+	for file: String in dir.get_files():
+		var ud_level: UDLevel = load(dir.get_current_dir() + "/" + file)
+		print(file, "after load")
+		_ud_levels.append(ud_level.duplicate())
+		print(file, "after duplicate")
+	print_stack()
+	return _ud_levels
 
 func save_to_disk() -> void:
 	var result := ResourceSaver.save(user_data, _FILE_NAME)
--- global_systems/user_data_system/user_progress.gd
+++ global_systems/user_data_system/user_progress.gd
@@ -1,14 +1,3 @@
 class_name UserProgress extends Resource
 
 @export var ud_levels: Array[UDLevel] 
-
-func _init() -> void:
-	_create_ud_levels()
-
-func _create_ud_levels():
-	var dir := DirAccess.open("res://data/ud_levels/data/")
-	assert(dir != null, "Could not open folder")
-	dir.list_dir_begin()
-	for file: String in dir.get_files():
-		var ud_level: UDLevel = load(dir.get_current_dir() + "/" + file)
-		ud_levels.append(ud_level.duplicate())
