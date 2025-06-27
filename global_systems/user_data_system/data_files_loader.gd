012054b5e413bd84c32230aea2a2a4c346ae78f2
diablohumastudioadmin <admin@diablohumastudio.com>
2025-06-27 15:35:58 -0500

    functions for loading from file in a central place

---
+++ global_systems/user_data_system/data_files_loader.gd
@@ -0,0 +1,42 @@
+class_name DataFilesLoader extends Node
+
+static func create_ud_levels_from_res_files() -> Array[UDLevel]:
+	var _ud_levels: Array[UDLevel]
+	var dir := DirAccess.open("res://data/ud_levels/data/")
+	assert(dir != null, "Could not open folder")
+	dir.list_dir_begin()
+	for file: String in dir.get_files():
+		var ud_level: UDLevel = load(dir.get_current_dir() + "/" + file)
+		_ud_levels.append(ud_level.duplicate())
+	return _ud_levels
+
+static func create_ud_achievements_from_res_files() -> Array[UDAchievement]:
+	var _ud_levels: Array[UDAchievement]
+	var dir := DirAccess.open("res://data/ud_achievements/data/")
+	assert(dir != null, "Could not open folder")
+	dir.list_dir_begin()
+	for file: String in dir.get_files():
+		var ud_level: UDAchievement = load(dir.get_current_dir() + "/" + file)
+		_ud_levels.append(ud_level.duplicate())
+	return _ud_levels
+
+static func get_achievements_from_res_files() -> Array[Achievement]:
+	var achievements: Array[Achievement]
+	var dir := DirAccess.open("res://data/achievements/data/")
+	assert(dir != null, "Could not open folder")
+	dir.list_dir_begin()
+	for file: String in dir.get_files():
+		var ud_level: Achievement = load(dir.get_current_dir() + "/" + file)
+		achievements.append(ud_level)
+	print(achievements)
+	return achievements
+
+static func get_conditions_from_res_files() -> Array[Condition]:
+	var conds: Array[Condition]
+	var dir := DirAccess.open("res://data/conditions/data/")
+	assert(dir != null, "Could not open folder")
+	dir.list_dir_begin()
+	for file: String in dir.get_files():
+		var cond: Condition = load(dir.get_current_dir() + "/" + file)
+		conds.append(cond)
+	return conds
