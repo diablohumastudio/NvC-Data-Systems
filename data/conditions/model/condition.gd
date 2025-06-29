2d4f75f1c142029052ba0ee3886727421b941098
diablohumastudioadmin <admin@diablohumastudio.com>
2025-06-29 10:11:24 -0500

    warning ignores for condition signal and var

--- data/conditions/model/condition.gd
+++ data/conditions/model/condition.gd
@@ -1,8 +1,10 @@
 class_name Condition extends Resource
 
+@warning_ignore("unused_signal")
 signal fullfilled(cond: Condition)
 
 @export var id: String
 
+@warning_ignore("unused_parameter")
 func evaluate(action: Action):
 	pass
