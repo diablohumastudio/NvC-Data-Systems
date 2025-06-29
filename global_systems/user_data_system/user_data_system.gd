a09efba4a899aa15eee6b2b8a67bd83712af3fe8
diablohumastudioadmin <admin@diablohumastudio.com>
2025-06-29 10:23:37 -0500

    userdatasystem fixed for register and select user working. Rearrange initializing, setting current user, creating user and saving user

--- global_systems/user_data_system/user_data_system.gd
+++ global_systems/user_data_system/user_data_system.gd
@@ -9,39 +9,55 @@ var users_credentials: UsersCredentials
 var current_user_data: UserData 
 
 func _ready() -> void:
-	initialize_current_user_data()
+	initialize_users_data()
 
-func initialize_current_user_data():
+func initialize_users_data():
 	if ResourceLoader.exists(_USERS_CREDENTIALS_FILE_PATH):
 		users_credentials = load(_USERS_CREDENTIALS_FILE_PATH)
-		var current_user_credentials: UserCredentials = users_credentials.credentials[users_credentials.current_user_index]
-		current_user_data = load(_USER_FILE_BASE + current_user_credentials.user_name + ".tres")
+		var current_usr_cred: UserCredentials = users_credentials.credentials[users_credentials.current_user_index]
+		current_user_data = load(_USER_FILE_BASE + current_usr_cred.user_name + ".tres")
 	else:
 		users_credentials = UsersCredentials.new()
 		var new_user_credentials: UserCredentials = UserCredentials.new()
-		current_user_data = create_user_data(new_user_credentials)
-		users_credentials.credentials.append(new_user_credentials)
-		users_credentials.current_user_index = 0
+		create_new_user(new_user_credentials)
 
-func create_user_data(user_credentials: UserCredentials) -> UserData:
-	var _user_data: UserData
-	_user_data = UserData.new()
-	_user_data.user_name = user_credentials.user_name
-	_user_data.progress.ud_levels = DataFilesLoader.create_ud_levels_from_res_files()
-	_user_data.ud_achievements.ud_achievements = DataFilesLoader.create_ud_achievements_from_res_files()
-	return _user_data
+func create_new_user(user_credentials: UserCredentials, set_to_current: bool = true) -> UserData:
+	users_credentials.credentials.append(user_credentials)
+
+	var new_user_data: UserData = UserData.new()
+	new_user_data.user_name = user_credentials.user_name
+	new_user_data.progress.ud_levels = DataFilesLoader.create_ud_levels_from_res_files()
+	new_user_data.ud_achievements.ud_achievements = DataFilesLoader.create_ud_achievements_from_res_files()
+	
+	if set_to_current:
+		var new_user_index: int = users_credentials.credentials.find(user_credentials)
+		users_credentials.current_user_index = new_user_index
+		current_user_data = new_user_data
+	
+	var result : Error = ResourceSaver.save(users_credentials, _USERS_CREDENTIALS_FILE_PATH)
+	assert(result == OK)
+	result = ResourceSaver.save(new_user_data, _USER_FILE_BASE + new_user_data.user_name + ".tres")
+	assert(result == OK)
+
+	return new_user_data
+
+func set_current_user(user_credentials: UserCredentials) -> void:
+	var new_user_index: int = users_credentials.credentials.find(user_credentials)
+	users_credentials.current_user_index = new_user_index
+	current_user_data = load(_USER_FILE_BASE + user_credentials.user_name + ".tres")
 
 func save_user_data_to_disk() -> void:
-	var result := ResourceSaver.save(current_user_data, _USER_FILE_BASE + current_user_data.user_name + ".tres")
+	var result : Error = ResourceSaver.save(users_credentials, _USERS_CREDENTIALS_FILE_PATH)
 	assert(result == OK)
-	result = ResourceSaver.save(users_credentials, _USERS_CREDENTIALS_FILE_PATH)
+	result = ResourceSaver.save(current_user_data, _USER_FILE_BASE + current_user_data.user_name + ".tres")
+	assert(result == OK)
 
 func get_property(property: PROPERTIES):
 	match property:
 		PROPERTIES.USERS_CREDENTIALS:
 			return users_credentials
 		PROPERTIES.USER_NAME:
-			return current_user_data.user.user_name
+			return current_user_data.user_name
 		PROPERTIES.UD_LEVELS:
 			return current_user_data.progress.ud_levels
 		PROPERTIES.ACHIEVEMENTS:
