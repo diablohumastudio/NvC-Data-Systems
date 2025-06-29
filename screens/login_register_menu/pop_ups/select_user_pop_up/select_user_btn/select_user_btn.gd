293d756ff771549396b0c2f587c933e0f66979ed
diablohumastudioadmin <admin@diablohumastudio.com>
2025-06-29 10:14:18 -0500

    select_user_popup implemented

--- screens/login_register_menu/pop_ups/select_user_pop_up/select_user_btn/select_user_btn.gd
+++ screens/login_register_menu/pop_ups/select_user_pop_up/select_user_btn/select_user_btn.gd
@@ -2,6 +2,18 @@ class_name SelectUserBtn extends Button
 
 @export var user_credentials: UserCredentials : set = _set_user_credentials
 
+var base_text: String
+
 func _set_user_credentials(new_value: UserCredentials) -> void:
 	user_credentials = new_value
-	self.text = "Select: '" + user_credentials.user_name+ "' user"
+	base_text = "Select: '" + user_credentials.user_name+ "' user"
+	text = base_text
+
+func _on_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		UDS.set_current_user(user_credentials)
+		modulate = Color.AQUAMARINE
+		text = base_text + "    SELECTED"
+	else: 
+		modulate = Color.WHITE
+		text = base_text
--- screens/login_register_menu/pop_ups/select_user_pop_up/select_user_pop_up.gd
+++ screens/login_register_menu/pop_ups/select_user_pop_up/select_user_pop_up.gd
@@ -1,10 +1,27 @@
-extends Control
+class_name SelectUserPopUp extends Control
+
+var new_button_group: ButtonGroup = ButtonGroup.new()
 
 func _ready() -> void:
-	for user_credential in UDS.current_user_data
+	var users_credentials: UsersCredentials = UDS.get_property(UDS.PROPERTIES.USERS_CREDENTIALS)
+	for child in %UsersContainer.get_children():
+		child.queue_free()
+	for ii in users_credentials.credentials.size():
+		var user_credentials: UserCredentials = users_credentials.credentials[ii]
+		var new_select_user_btn: SelectUserBtn = load("res://screens/login_register_menu/pop_ups/select_user_pop_up/select_user_btn/select_user_btn.tscn").instantiate()
+		new_select_user_btn.user_credentials = user_credentials
+		new_select_user_btn.button_group = new_button_group
+		new_select_user_btn.pressed.connect(_on_select_user_btn_pressed)
+		if ii == users_credentials.current_user_index:
+			new_select_user_btn.button_pressed = true
+		%UsersContainer.add_child(new_select_user_btn)
+
+func _on_select_user_btn_pressed():
+	%AceptBtn.show()
+	%CloseBtn.hide()
 
 func _on_close_btn_pressed() -> void:
 	hide()
 
-func _on_go_back_pressed() -> void:
-	hide()
+func _on_acept_btn_pressed() -> void:
+	SMS.change_scene(GC.SCREENS_UIDS.WELCOME_MENU)
