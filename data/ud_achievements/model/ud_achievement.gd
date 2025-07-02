23ccbc119d542a87f1790697449ab7a6ec1e4ecb
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-02 09:06:31 -0500

    godot bug fix changing name in tracking cond in achievement

--- data/ud_achievements/model/ud_achievement.gd
+++ data/ud_achievements/model/ud_achievement.gd
@@ -8,18 +8,18 @@ signal achieved(id: Achievement.IDs)
 @export_storage var porcentage_achieved: int = 0
 
 @export var conditions: Array[Condition]: set = _set_conditions
-@export_storage var conditions_trak: Array = []
+@export_storage var conditions_track: Array = []
 
 func _set_conditions(new_value: Array[Condition]):
 	conditions = new_value
 	if !conditions: return
 	for condition in conditions:
-		conditions_trak.append(false)
+		conditions_track.append(false)
 		condition.fullfilled.connect(_on_condition_fullfilled)
 
 func _on_condition_fullfilled(condition: Condition):
-	conditions_trak[conditions.find(condition)] = true
-	for cond_tr in conditions_trak:
+	conditions_track[conditions.find(condition)] = true
+	for cond_tr in conditions_track:
 		if cond_tr == false:
 			return
 	if !is_achieved:
