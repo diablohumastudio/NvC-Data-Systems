44ec2328954db770fb4ceab677fe0b71b1cd63ac
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-02 09:06:52 -0500

    just some order in ACS

--- global_systems/action_condition_system/action_condition_system.gd
+++ global_systems/action_condition_system/action_condition_system.gd
@@ -5,9 +5,10 @@ var conditions: Array[Condition]
 func _ready() -> void:
 	conditions = DataFilesLoader.get_conditions_from_res_files()
 
-func _evaluate_conditions(action: Action):
-	for cond in conditions:
-		cond.evaluate(action)
-
 func set_action(action: Action):
 	_evaluate_conditions(action)
+
+func _evaluate_conditions(action: Action):
+	for cond in conditions:
+		if cond.type == action.type:
+			cond.evaluate(action)
