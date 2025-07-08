69aca2d770431f0a3fab7f6eeacfa10816d08f89
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-08 18:04:44 -0500

    removed premenu screen from GC

--- global_systems/global_constants/global_constants.gd
+++ global_systems/global_constants/global_constants.gd
@@ -3,7 +3,6 @@ extends Node
 const TOTAL_NUMBER_OF_CANONS: int = 5
 
 const SCREENS_UIDS: Dictionary = {
-	"PREMENU": "uid://b1oj3ekgb440y",
 	"MENU": "uid://iinlpvu7gc2e",
 	"GAME": "uid://c2b4jldseu5vs",
 	"ACHIEVEMENTS": "uid://bbsi2yvced0xh",
