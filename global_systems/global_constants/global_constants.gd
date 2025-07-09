6b0a0b74c9bad8d6d73c1fafd19c4d19086caa43
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-09 09:51:04 -0500

    order some folders/files

--- global_systems/global_constants/global_constants.gd
+++ global_systems/global_constants/global_constants.gd
@@ -10,3 +10,14 @@ const SCREENS_UIDS: Dictionary = {
 	"LOGIN_REGISTER_MENU": "uid://cxxmq32l3eqdc",
 	"ALLIES_MARKET": "uid://d25cnc38vhdvn"
 }
+
+const DATA_FOLDERS_PATHS: Dictionary = {
+	"UD_LEVELS": "res://data/levels/user_data/data/",
+	"UD_ALLIES":"res://data/game_characters/allies/user_data/data/",
+	"UD_ACHIEVEMENTS":"res://data/achievements/user_data/data/",
+	"RES_LEVELS":"res://data/levels/res_data/data/",
+	"RES_ALLIES":"res://data/game_characters/allies/res_data/data/",
+	"RES_ACHIEVEMENTS":"res://data/achievements/res_data/data/",
+	"CONDITIONS":"res://data/acs_system/conditions/data/",
+	"STATE_CHANGERS":"res://data/acs_system/state_changers/data/",
+}
