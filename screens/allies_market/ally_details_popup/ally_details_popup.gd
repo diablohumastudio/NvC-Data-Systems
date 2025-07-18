955e3b6502cd2b860d644e6a4254593e07c7c2a0
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-17 19:35:01 -0500

    previous linear allies unlocking/buying system replace with flexible one. UdAlly added arrays for unlocked and buyed levels (only ids).

--- screens/allies_market/ally_details_popup/ally_details_popup.gd
+++ screens/allies_market/ally_details_popup/ally_details_popup.gd
@@ -17,8 +17,13 @@ func display_levels() -> void:
 		child.queue_free()
 	for level in ally.ud_ally.levels_conditions:
 		var new_level_presenter: AllyLevelPresenter = load("res://screens/allies_market/ally_details_popup/ally_level_presenter/ally_level_presenter.tscn").instantiate()
-		new_level_presenter.level = level
+		new_level_presenter.ally_level = level
+		new_level_presenter.level_buyed.connect(update_all_level_presenters)
 		%AllylevelsPresContainer.add_child(new_level_presenter)
+
+func update_all_level_presenters():
+	for child in %AllylevelsPresContainer.get_children():
+		(child as AllyLevelPresenter)._update_visuals()
 
 func _on_close_btn_pressed() -> void:
 	hide()
--- screens/allies_market/ally_details_popup/ally_level_presenter/ally_level_presenter.gd
+++ screens/allies_market/ally_details_popup/ally_level_presenter/ally_level_presenter.gd
@@ -1,11 +1,13 @@
 class_name AllyLevelPresenter extends MarginContainer
 
-var level: AllyLevel : set = _set_level
+signal level_buyed
+
+var ally_level: AllyLevel : set = _set_level
 var is_unlocked: bool : set = _set_is_unlocked
 
 func _set_level(new_value: AllyLevel):
-	level = new_value
-	if level:
+	ally_level = new_value
+	if ally_level:
 		_update_visuals()
 
 func _set_is_unlocked(new_value: bool):
@@ -14,10 +16,14 @@ func _set_is_unlocked(new_value: bool):
 		_update_visuals()
 
 func _update_visuals():
-	%AllyLevelName.text = level.level_id
-	%AllyLevelPrice.text = str(level.price)
-	%AllyLevelIsUnlocked.text = str(is_unlocked)
+	var ally: Ally = DataFilesLoader.get_allies_from_res_file_by_id(ally_level.ally_id)
+	%AllyLevelName.text = ally_level.level_id
+	%AllyLevelPrice.text = str(ally_level.price)
+	%AllyLevelIsUnlocked.text = str(ally.ud_ally.is_level_unlocked(ally_level.level_id))
+	%AllyLevelIsBuyed.text = str(ally.ud_ally.is_level_buyed(ally_level.level_id))
 
 func _on_buy_ally_level_btn_pressed() -> void:
-	print(level.locked)
-	level.buyed = true
+	var ally: Ally = DataFilesLoader.get_allies_from_res_file_by_id(ally_level.ally_id)
+	ACS.set_action(Action.new(Action.TYPES.BUYED_ALLY_LEVEL, Action.PayBuyedAllyLevel.new(ally_level.level_id, ally_level.ally_id)))
+	_update_visuals()
+	level_buyed.emit()
