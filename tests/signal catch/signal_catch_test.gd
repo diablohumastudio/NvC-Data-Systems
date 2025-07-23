d432b71236746c357d5c753913b765e77bff4d17
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-23 17:43:23 -0500

    little test project for understanding if conections with of same signal to same function conects again if have different bind. Spoiler: NO

--- tests/signal catch/signal_catch_test.gd
+++ tests/signal catch/signal_catch_test.gd
@@ -4,8 +4,26 @@ var my_res : ResSignalEmitter
 var res_listener_1: SignalResListener = SignalResListener.new()
 var res_listener_2: SignalResListener = SignalResListener.new()
 
+var res_listener_out_1: SignalResListenerOutConection = SignalResListenerOutConection.new()
+var res_listener_out_2: SignalResListenerOutConection = SignalResListenerOutConection.new()
+
+var res_listener_out_3: SignalResListenerOutConection = SignalResListenerOutConection.new()
+
+var res_listener_out_4: SignalResListenerOutConection = SignalResListenerOutConection.new()
+
+var test_res_4: Resource = Resource.new()
+var test_res_4_1: Resource = Resource.new()
+
 func _ready() -> void:
 	my_res = load("res://tests/signal catch/res_signal_emitter/data/my_res_signal_emmiter_file.tres")
-
+	my_res.i_have_emitted.connect(res_listener_out_1.on_my_res_signal_emitter_i_have_emitted)
+	my_res.i_have_emitted.connect(res_listener_out_2.on_my_res_signal_emitter_i_have_emitted)
+	
+	my_res.i_have_emitted.connect(res_listener_out_3.on_my_res_signal_emitter_i_have_emitted.bind(3))
+	my_res.i_have_emitted.connect(res_listener_out_3.on_my_res_signal_emitter_i_have_emitted.bind(4))
+	
+	my_res.i_have_emitted.connect(res_listener_out_4.on_my_res_signal_emitter_i_have_emitted.bind(test_res_4))
+	my_res.i_have_emitted.connect(res_listener_out_4.on_my_res_signal_emitter_i_have_emitted.bind(test_res_4_1))
+	
 func _on_emit_signal_from_res_btn_pressed() -> void:
 	my_res.i_have_emitted.emit()
---
+++ tests/signal catch/signal_res_listener_out_conection/signal_res_listener_out_conection.gd
@@ -0,0 +1,4 @@
+class_name SignalResListenerOutConection extends Resource
+
+func on_my_res_signal_emitter_i_have_emitted(_placeholder_argument = 1):
+	printt("i recieved even been conected from outside", _placeholder_argument)
