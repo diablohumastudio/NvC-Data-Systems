0d3de7f38753a7694526db7ecbf1f4c1275e3869
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-25 10:55:39 -0500

    now that is a function, no need to validate UDS existence

--- data/levels/res_data/model/level.gd
+++ data/levels/res_data/model/level.gd
@@ -9,9 +9,4 @@ enum IDs {Level1, Level2, Level2p, Level3, LevelEx, LevelEx2, Level4}
 @export var unlockd_by_default: bool = false
 
 func get_ud_level() -> UDLevel:
-	var new_ud_level : UDLevel
-
-	if !UDS : return
-	new_ud_level = UDS.get_ud_level_by_id(id)
-
-	return new_ud_level
+	return UDS.get_ud_level_by_id(id)
