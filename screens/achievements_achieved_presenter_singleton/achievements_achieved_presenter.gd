c8a7c73126d0058830fa89c233bb8f8fc58b1dbd
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-25 11:43:23 -0500

    loading pksc one time in achievement achieved presenter

--- screens/achievements_achieved_presenter_singleton/achievements_achieved_presenter.gd
+++ screens/achievements_achieved_presenter_singleton/achievements_achieved_presenter.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var achivement_achieved_presenter_pksc: PackedScene = load("res://screens/achievements_achieved_presenter_singleton/achievement_achieved_presenter/achievement_achieved_presenter.tscn")
+
 func _ready() -> void:
 	conect_ud_achievements()
 	UDS.current_user_changed.connect(_on_UDS_current_user_changed)
@@ -16,7 +18,7 @@ func conect_ud_achievements():
 func _on_ud_achievement_achieved(id: Achievement.IDs):
 	var achievement = DataFilesLoader.get_achiev_from_res_file_by_id(id)
 	var new_achivement_achieved_presenter: AchievementAchievedPresenter
-	new_achivement_achieved_presenter = load("res://screens/achievements_achieved_presenter_singleton/achievement_achieved_presenter/achievement_achieved_presenter.tscn").instantiate()
+	new_achivement_achieved_presenter = achivement_achieved_presenter_pksc.instantiate()
 	%AchievementsContainer.add_child(new_achivement_achieved_presenter)
 	new_achivement_achieved_presenter.achievement = achievement
 	await get_tree().create_timer(1.5).timeout
