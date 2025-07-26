3cddcc1eb1cf21695d1ccaa6bb216dd0cff43d70
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-25 19:40:23 -0500

    state changer changed for new User Data structure

--- data/acs_system/state_changers/model/sc_buyed_ally_level.gd
+++ data/acs_system/state_changers/model/sc_buyed_ally_level.gd
@@ -8,8 +8,5 @@ func change_state(action: Action):
 	var ally_id = payload.ally_id
 	var ally_level_id = payload.ally_level_id
 
-	for ud_ally in UDS.current_user_data.allies_inventory.ud_allies:
-		if ud_ally.id == ally_id:
-			var is_ally_level_buyed = ud_ally.buyed_levels.has(ally_level_id)
-			if !is_ally_level_buyed:
-				ud_ally.buyed_levels.append(ally_level_id)
+	var ud_ally_level: UDAllyLevel = UDS.get_ud_ally_level_by_id_in_ally(ally_level_id,ally_id)
+	ud_ally_level.buyed = true
