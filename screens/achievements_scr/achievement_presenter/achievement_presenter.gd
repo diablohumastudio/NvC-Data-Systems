1442004897a8139b8346f90ec8d1c121baf7ac0d
diablohumastudioadmin <admin@diablohumastudio.com>
2025-07-28 17:14:31 -0500

    achievements now show the porcentage advance

--- screens/achievements_scr/achievement_presenter/achievement_presenter.gd
+++ screens/achievements_scr/achievement_presenter/achievement_presenter.gd
@@ -4,7 +4,12 @@ class_name AchievementPresenter extends HBoxContainer
 
 func _set_achievement(new_value: Achievement):
 	achievement = new_value
+	var porcentage_achived: int =  0
+	var total_conditions: int = achievement.conditions.size()
+	var achieved_conditions: int = achievement.get_saved_ud_achievement().fullfilled_conditions.size()
+	porcentage_achived = int(float(achieved_conditions) / float(total_conditions) * 100)
 	%AchievName.text = achievement.achievement_name
+	%PorcentageFullfiled.text = "(" + str(achieved_conditions) + "/" + str(total_conditions) + "=" + str(porcentage_achived) + "%)" 
 	%AchievFullfilled.text = str(achievement.get_saved_ud_achievement().is_achieved)
 	if achievement.get_saved_ud_achievement().is_achieved:
 		%AchievFullfilled.modulate = Color.GREEN
