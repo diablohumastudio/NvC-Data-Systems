918757cbe6e4929692c62cd8a22c9c3fb40040e7
diablohumastudioadmin <admin@diablohumastudio.com>
2025-08-05 17:39:42 -0500

    allies selector selects and unselects ally

--- screens/game/allies_selector/ally_selector_card/ally_selector_card.gd
+++ screens/game/allies_selector/ally_selector_card/ally_selector_card.gd
@@ -7,7 +7,9 @@ var ally: Ally
 func _ready() -> void:
 	if !ally: return
 	%AllyName.text = ally.ally_name
-	%AllySelCardthumbnailBtn.texture_normal = ally.base_level.ally_selector_thumbnail
+	%AllySelCardBtn.texture_normal = ally.base_level.ally_selector_thumbnail
+	%AllySelCardBtn.set_meta("ally", ally)
 
-func _on_ally_sel_cardthumbnail_btn_pressed() -> void:
-	selected.emit()
+func _on_ally_sel_card_btn_toggled(toggled_on: bool) -> void:
+	if toggled_on: modulate = Color.GREEN
+	else: modulate = Color.WHITE
