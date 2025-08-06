6c8cd8ddac0e7ef88e624eb5d965ef94b30835ee
diablohumastudioadmin <admin@diablohumastudio.com>
2025-08-05 19:12:19 -0500

    new node GameStats for stats variables of game

--- screens/game/game.gd
+++ screens/game/game.gd
@@ -1,8 +1,6 @@
 class_name Game extends Control
 
 var level: Level : set = _set_level
-var enemies_killed: int = 0
-var canons_alive: int = GC.TOTAL_NUMBER_OF_CANONS
 
 func _set_level(new_value: Level) -> void:
 	level = new_value
@@ -25,20 +23,20 @@ func _update_kill_enemy_tot_label():
 	%KillEnemyTotalLbl.text = str(UDS.get_property(UDS.PROPERTIES.ENEMIES_KILLED)) + " Enms Klld in Totl"
 
 func _update_kill_enemy_label():
-	%KillEnemyLbl.text = str(enemies_killed) + " Enms Klld in lvl"
+	%KillEnemyLbl.text = str(%GameStats.enemies_killed) + " Enms Klld in lvl"
 
 func _on_win_btn_pressed() -> void:
-	ACS.set_action(Action.new(Action.TYPES.LV_COMPLTD_ALL_CANONS, Action.PayLvlComplAllCanons.new(level.id, canons_alive)))
+	ACS.set_action(Action.new(Action.TYPES.LV_COMPLTD_ALL_CANONS, Action.PayLvlComplAllCanons.new(level.id, %GameStats.canons_alive)))
 	ACS.set_action(Action.new(Action.TYPES.LV_COMPLTD, Action.PayLvCompl.new(level.id)))
 
 func _on_go_back_btn_pressed() -> void:
 	SMS.change_scene(GC.SCREENS_UIDS.MENU)
 
 func _on_kill_enemy_btn_pressed() -> void:
-	enemies_killed += 1
-	ACS.set_action(Action.new(Action.TYPES.ENEMY_KILLED, Action.PayEnemKilled.new(enemies_killed)))
+	%GameStats.enemies_killed += 1
+	ACS.set_action(Action.new(Action.TYPES.ENEMY_KILLED, Action.PayEnemKilled.new(%GameStats.enemies_killed)))
 	_update_kill_enemy_label()
 
 func _on_canon_killed_btn_pressed() -> void:
-	canons_alive -= 1
-	%CanonsLeft.text = str(canons_alive) + " Canons Left"
+	%GameStats.canons_alive -= 1
+	%CanonsLeft.text = str(%GameStats.canons_alive) + " Canons Left"
---
+++ screens/game/game_stats/game_stats.gd
@@ -0,0 +1,4 @@
+class_name GameStats extends Node
+
+var enemies_killed: int = 0
+var canons_alive: int = GC.TOTAL_NUMBER_OF_CANONS
