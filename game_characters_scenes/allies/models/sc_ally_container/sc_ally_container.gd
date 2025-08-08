fa05bad79687bab11c2c8a75c2fb40976233ced1
Ferndando DHS <fernandobarahonad@diablohumastudio.com>
2025-08-08 12:25:16 -0500

    ScAllyContainer for each ally. Not yet completed

--- game_characters_scenes/allies/models/sc_ally_container/sc_ally_container.gd
+++ game_characters_scenes/allies/models/sc_ally_container/sc_ally_container.gd
@@ -4,15 +4,12 @@ var level: AllyLevel: set = _set_level
 var ally: Ally
 var hp: int
 
-var scene_ally: ScAlly
-
 func _set_level(new_value: AllyLevel):
 	level = new_value
 	if is_inside_tree():
 		set_text_and_scene()
 
 func _ready() -> void:
-	%AllyUpgradeMenu.ally = ally
 	%AllyUpgradeMenu.level_changed.connect(on_ally_upgrade_menu_level_changed)
 	set_text_and_scene()
 
@@ -21,13 +18,9 @@ func on_ally_upgrade_menu_level_changed(_level: AllyLevel):
 
 func set_text_and_scene():
 	%AllyNameAndLevelLabel.text = "Type: " + ally.ally_name + "\n Level " + str(level.level_id)
-	scene_ally = level.scene.instantiate()
-	get_node("ScAlly").free()
-	add_child(scene_ally)
-	call_deferred("_change_scally_name")
 
-func _change_scally_name():
-	scene_ally.name = "ScAlly"
-
-func _on_show_upgrade_popup_btn_pressed() -> void:
-	%AllyUpgradeMenu.visible = true
+func _on_select_ally_btn_button_up() -> void:
+	if GSS.removing_ally_state: 
+		self.queue_free()
+		GSS.removing_ally_state = false
+	else: %AllyUpgradeMenu.visible = true
--- screens/game/components/terrain_grid/cell/cell.gd
+++ screens/game/components/terrain_grid/cell/cell.gd
@@ -1,25 +1,25 @@
 class_name Cell extends Button
 
-var placed_ally: Ally
 const SC_ALLY_CONTAINER_PKSC: PackedScene = preload("uid://bhw8iapb537a1")
 
 func _on_pressed() -> void:
-	if GSS.removing_ally_state:
-		if get_child_count() > 0:
-			get_child(0).queue_free()
-			placed_ally = null
-			GSS.removing_ally_state = false
-	else:
-		if get_child_count() == 0:
-			_add_ally()
-			GSS.ally_to_place = null
+	_validate_children()
+	print( !GSS.removing_ally_state , !is_ocupied() , GSS.ally_to_place)
+	if !GSS.removing_ally_state and !is_ocupied() and GSS.ally_to_place:
+		_add_ally()
+		GSS._ally_just_placed.emit(GSS.ally_to_place)
+		GSS.ally_to_place = null
+
+func is_ocupied() -> bool:
+	return !get_child_count() == 0
+
+func _validate_children():
+	if get_child_count() > 1:
+		push_error("Cell MUST NOT have more than one children")
+	elif get_child_count() == 1 and !get_child(0) is ScAllyContainer:
+			push_error("Cell MUST NOT contain children of diferent class than ScALlyContainer")
 
 func _add_ally():
-	if !GSS.ally_to_place: return
-	
-	placed_ally = GSS.ally_to_place
-	GSS._ally_just_placed.emit(placed_ally)
-	
 	var new_ally_scene_container: ScAllyContainer = SC_ALLY_CONTAINER_PKSC.instantiate()
 	new_ally_scene_container.ally = GSS.ally_to_place
 	new_ally_scene_container.level = GSS.ally_to_place.base_level
