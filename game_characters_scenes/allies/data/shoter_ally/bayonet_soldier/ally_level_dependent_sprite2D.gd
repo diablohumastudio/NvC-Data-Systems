b0e1695d5e14c0c7596848d39116527d00b4a687
Ferndando DHS <fernandobarahonad@diablohumastudio.com>
2025-08-13 11:02:10 -0500

    node2d dependant from allylevels in scally created

--- game_characters_scenes/allies/data/shoter_ally/bayonet_soldier/ally_level_dependent_sprite2D.gd
+++ game_characters_scenes/allies/data/shoter_ally/bayonet_soldier/ally_level_dependent_sprite2D.gd
@@ -1,11 +1,11 @@
-extends Sprite2D
+class_name AllyLevelDependentSprite2D extends Sprite2D
 
 @export var levels_modifiers_textures: Dictionary[AllyLevel, Texture2D]
 
 func _ready() -> void:
 	add_to_group(str(owner))
 
-func _on_ally_btn_pressed(levels: Array[AllyLevel]):
+func set_levels(levels: Array[AllyLevel]):
 	printt("el boton fue presionado y la funcion del nodo activada en un nodo del aliado: ", owner)
 	for level in levels:
 		if levels_modifiers_textures.has(level):
