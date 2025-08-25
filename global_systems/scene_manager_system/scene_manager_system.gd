fe67627ce8519f02e544408b4eb7c8a96bee7d3b
diablohumastudioadmin <admin@diablohumastudio.com>
2025-08-25 13:40:40 -0500

    scene manager system now works for not global (get_tree().root) also

--- global_systems/scene_manager_system/scene_manager_system.gd
+++ global_systems/scene_manager_system/scene_manager_system.gd
@@ -1,15 +1,19 @@
 extends Node
 
-func change_scene(scene: PackedScene, arguments: Dictionary = {}) -> void:
-	var current_scene: Node = get_tree().current_scene
-	print(current_scene)
-	var new_scene: Node = scene.instantiate()
+func change_scene(scene: PackedScene, arguments: Dictionary = {}, current_scene: Node = get_tree().current_scene) -> void:
+	var new_scene: Node 
+	
+	assert(scene.can_instantiate())
+	new_scene = scene.instantiate()
 	
 	for key in arguments:
 		if key in new_scene:
 			new_scene[key] = arguments[key]
 	if new_scene.has_method("_sms_initialize"): new_scene._sms_initialize()
 	
-	get_tree().root.add_child(new_scene)
-	get_tree().current_scene = new_scene
+	var current_scene_name: String = current_scene.name
+	current_scene.name = "OldScene"
+	new_scene.name = current_scene_name
+	current_scene.get_parent().add_child(new_scene)
+	if current_scene == get_tree().current_scene: get_tree().current_scene = new_scene
 	current_scene.queue_free()
