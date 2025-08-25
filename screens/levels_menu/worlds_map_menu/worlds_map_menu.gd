ae5635dca1b0c7936547b281a1ab9e4a77b09497
diablohumastudioadmin <admin@diablohumastudio.com>
2025-08-25 13:41:33 -0500

    worlds_map_menu now works for going to all other menus

--- screens/levels_menu/worlds_map_menu/worlds_map_menu.gd
+++ screens/levels_menu/worlds_map_menu/worlds_map_menu.gd
@@ -11,21 +11,18 @@ func _ready() -> void:
 		%AnimationPlayerTransition.play("goto_stalingrad_winter_level_map_menu", -1, -1.5, true)
 
 func _on_go_back_button_pressed() -> void:
-	var main_menu: MainMenu = SLS.get_scene("uid://cflg2ypu25js2").instantiate()
+	var main_menu: PackedScene = SLS.get_scene("uid://cflg2ypu25js2")
 	main_menu._entering_from_worlds_map_menu = true
-	var menus_container:SceneContainer = SMS.get_container("Main") as SceneContainer
-	menus_container.goto_scene(main_menu,SM.ANIMATIONS.NONE,SM.ANIMATIONS.NONE)
+	SMS.change_scene(main_menu)
 
 func _on_stalingrad_summer_button_pressed() -> void:
-	var stalingrad_summer_levels_menu: StalingradSummerLevelsMenu = SLS.get_scene("uid://bnl38rkw31r66").instantiate()
-	var menus_container:SceneContainer = SMS.get_container("Main") as SceneContainer
+	var stalingrad_summer_levels_menu: PackedScene = SLS.get_scene("uid://bnl38rkw31r66")
 	%AnimationPlayerTransition.play("goto_stalingrad_summer_level_map_menu", -1, 1.5)
 	await %AnimationPlayerTransition.animation_finished
-	menus_container.goto_scene(stalingrad_summer_levels_menu,SM.ANIMATIONS.NONE,SM.ANIMATIONS.NONE)
+	SMS.change_scene(stalingrad_summer_levels_menu)
 
 func _on_stalingrad_winter_button_pressed() -> void:
-	var stalingrad_winter_levels_menu: StalingradWinterLevelsMenu = SLS.get_scene("uid://gci4nehonton").instantiate()
-	var menus_container:SceneContainer = SMS.get_container("Main") as SceneContainer
+	var stalingrad_winter_levels_menu: PackedScene = SLS.get_scene("uid://gci4nehonton")
 	%AnimationPlayerTransition.play("goto_stalingrad_winter_level_map_menu", -1, 1.5)
 	await %AnimationPlayerTransition.animation_finished
-	menus_container.goto_scene(stalingrad_winter_levels_menu,SM.ANIMATIONS.NONE,SM.ANIMATIONS.NONE)
+	SMS.change_scene(stalingrad_winter_levels_menu)
