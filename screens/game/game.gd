dce09ffe6974d2f3cf46041e56e46b5097723fe2
diablohumastudioadmin <admin@diablohumastudio.com>
2025-08-26 12:13:26 -0500

    return from game to stalingrad summer. Still needs to remember last menu so will go to winter or other world menu

--- screens/game/game.gd
+++ screens/game/game.gd
@@ -1,4 +1,4 @@
 class_name Game extends Control
 
 func _on_go_back_btn_pressed() -> void:
-	SMS.change_scene(load(GC.SCREENS_UIDS.MENU))
+	SMS.change_scene(load(GC.SCREENS_UIDS.STALINGRAD_SUMMER_LEVELS_MENU))
