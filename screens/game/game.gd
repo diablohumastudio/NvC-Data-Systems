211fb8803603476ee78f38eacae2f91b9a539b0a
diablohumastudioadmin <admin@diablohumastudio.com>
2025-08-27 16:14:32 -0500

    changed game for working with old test screens and with new final project

--- screens/game/game.gd
+++ screens/game/game.gd
@@ -1,4 +1,7 @@
 class_name Game extends Control
 
 func _on_go_back_btn_pressed() -> void:
-	SMS.change_scene(load(GC.SCREENS_UIDS.STALINGRAD_SUMMER_LEVELS_MENU))
+	SMS.change_scene(load(GC.SCREENS_UIDS.MAIN_MENU))
+
+func _on_go_back_to_test_btn_pressed() -> void:
+	SMS.change_scene(load(GC.SCREENS_UIDS.MENU))
