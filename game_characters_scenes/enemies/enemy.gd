723fdbcc7ccc603c8f8efa066e616542330fb282
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-01 17:06:44 -0500

    enemy_scene now have type property so it can be setted in editor the desired type of enemy

--- game_characters_scenes/enemies/enemy.gd
+++ game_characters_scenes/enemies/enemy.gd
@@ -1,8 +1,8 @@
-class_name Enemy extends GameCharacter
+class_name EnemyScene extends GameCharacter
 
-# Enemy-specific properties (inherits hp, current_hp, accuracy_points, DAMAGE_TYPES from GameCharacter)
+var type: TYPES
+enum TYPES {HandGunGerman}
 
-# Override the damage and death interface for enemy-specific behavior
 func receive_damage(damage_points: float, _damage_type: GameCharacter.DAMAGE_TYPES):
 	super.receive_damage(damage_points, _damage_type)
 
