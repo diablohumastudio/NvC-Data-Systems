2bc2db794b1fbd417e49a0a5e430bde5eef62781
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-02 11:36:30 -0500

    scene_manager_system improved

--- screens/start_menus/about_menu/popup_presenter/popup_presenter.gd
+++ screens/start_menus/about_menu/popup_presenter/popup_presenter.gd
@@ -3,7 +3,7 @@ class_name PopupPresenter extends Node2D
 
 func display_popup_scene(_selected_popup_scene: PackedScene) -> void:
 	var selected_popup_scene : PackedScene = _selected_popup_scene
-	SMS.change_scene(selected_popup_scene, {}, $PopupScene1)
+	SMS.change_scene(selected_popup_scene, {}, $PopupScene1, true)
 
 func display_exit_popup_animation() -> void:
 	var popup_scene : Control = get_child(0)
