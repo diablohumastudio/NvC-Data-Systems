9b8442b4251360ef110542a31dfa6c2f0bd90f0e
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-04 18:58:12 -0500

    Dbug game btn for emulate gameover

--- screens/game/debug_components/dbug_win_loose_btns/dbug_win_loose_btns.gd
+++ screens/game/debug_components/dbug_win_loose_btns/dbug_win_loose_btns.gd
@@ -1,7 +1,10 @@
-class_name DBugWinBtn extends Button
+class_name DBugWinBtn extends HBoxContainer
 
-func _on_pressed() -> void:
+func _on_win_btn_pressed() -> void:
 	ACS.set_action(Action.new(Action.TYPES.LV_COMPLTD_ALL_CANONS, PayLvlComplAllCanons.new(GSS.level.id, GSS.canons_alive)))
 	ACS.set_action(Action.new(Action.TYPES.LV_COMPLTD, PayLvCompl.new(GSS.level.id)))
 	GSS.game_just_won.emit()
 	UDS.save_user_data_to_disk()
+
+func _on_loose_btn_pressed() -> void:
+	GSS.enemy_reached_last_column.emit()
