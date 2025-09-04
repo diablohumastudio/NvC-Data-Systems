82b7ed93fa3da0f8e2c04839ed9f47b54e2ae695
Fernando <fernandobarahonad@diablohumastudio.com>
2025-09-03 22:04:13 -0500

    balance displayer compontent added. balance var and balance_changed signal added to gss. dbug_balance_change component added to test.

---
+++ screens/game/debug_components/dbug_balance_change_hud/dbug_balance_change_hud.gd
@@ -0,0 +1,9 @@
+extends HBoxContainer
+
+
+func _on_add_50_btn_pressed() -> void:
+	GSS.add_value_to_balance(50)
+
+
+func _on_remove_50_btn_pressed() -> void:
+	GSS.substract_value_from_balance(50)
--- screens/game/game_state_system/game_state_system.gd
+++ screens/game/game_state_system/game_state_system.gd
@@ -5,7 +5,7 @@ signal ally_just_placed(ally: AllyData)
 @warning_ignore("unused_signal")
 signal game_just_won
 signal animation_trigered(animation_type: ANIMATION_TYPES)
-
+signal balance_changed(balance: int)
 signal _removing_ally_state_changed(state: bool)
 
 enum ANIMATION_TYPES {
@@ -21,6 +21,17 @@ var canons_alive: int
 var enemies_killed: int
 var ally_to_place: AllyData
 var removing_ally_state: bool: set = _set_removing_ally_state
+var balance: int : set = _set_balance
+
+func _set_balance(new_value):
+	balance = new_value
+	balance_changed.emit(balance)
+
+func add_value_to_balance(value_to_add:int):
+	balance += value_to_add
+
+func substract_value_from_balance(value_to_substract:int):
+	balance -= value_to_substract
 
 func _set_removing_ally_state(new_value):
 		removing_ally_state = new_value
