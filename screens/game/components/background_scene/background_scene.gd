6eeb23cdec2d0c1d4215fd645b5a386bd37d6e0c
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-03 19:09:46 -0500

    level added properties for background scene and background position. Game implement changing background depending on leveldata and GSS changed for trigering animations.
    Everything working

--- screens/game/components/background_scene/background_scene.gd
+++
@@ -1,1 +0,0 @@
-class_name BackgroundScene extends Control
--- screens/game/game.gd
+++ screens/game/game.gd
@@ -7,6 +7,10 @@ func _set_level(new_value: LevelData) -> void:
 	GSS.level = level
 	GSS.reset_values()
 
+func _ready() -> void:
+	SMS.change_scene(load(level.background_path), {}, $BackgroundScene, true)
+	$BackgroundScene.position.x = level.background_position
+
 func _on_go_back_btn_pressed() -> void:
 	SMS.change_scene(load(GC.SCREENS_UIDS.MAIN_MENU))
 
