c2c8fe3a86dfd9312ec591157597eb3d91e73034
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-03 19:08:39 -0500

    debug for ttrigering animation of background

--- screens/game/debug_components/dbug_trigger_animation_hud/dbug_trigger_animation_hud.gd
+++ screens/game/debug_components/dbug_trigger_animation_hud/dbug_trigger_animation_hud.gd
@@ -2,4 +2,4 @@ class_name DbugTriggerAnimationBtn extends Button
 
 func _on_pressed() -> void:
 	var anim_type: GSS.ANIMATION_TYPES = GSS.ANIMATION_TYPES[%TextEdit.text]
-	GSS.animation_trigered.emit(anim_type)
+	GSS.animation_trigered.emit(GSS.ANIMATION_TYPES.LAST_WAVE_STARTED)
