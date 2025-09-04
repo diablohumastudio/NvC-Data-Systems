576f15053e9f66061d4861130ada7264af4d0f60
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-03 19:08:12 -0500

    scene_manager_system fixed to let new_scene in same position (index in parent) as old one

--- global_systems/scene_manager_system/scene_manager_system.gd
+++ global_systems/scene_manager_system/scene_manager_system.gd
@@ -2,7 +2,7 @@ extends Node
 
 func change_scene(new_packed_scene: PackedScene, arguments: Dictionary = {}, current_scene: Node = get_tree().current_scene, is_same_name: bool = false) -> void:
 	var new_scene: Node 
-
+	
 	assert(new_packed_scene.can_instantiate())
 	new_scene = new_packed_scene.instantiate()
 
@@ -18,6 +18,12 @@ func change_scene(new_packed_scene: PackedScene, arguments: Dictionary = {}, cur
 		current_scene.name = "OldScene"
 		new_scene.name = current_scene_name
 
+	var old_scene_index_in_parent : int= current_scene.get_index()
 	current_scene.get_parent().add_child(new_scene)
+	new_scene.get_parent().move_child(new_scene, old_scene_index_in_parent)
+	
+	if current_scene.is_unique_name_in_owner(): 
+		new_scene.unique_name_in_owner = true
+	
 	if current_scene == get_tree().current_scene: get_tree().current_scene = new_scene
 	current_scene.queue_free()
