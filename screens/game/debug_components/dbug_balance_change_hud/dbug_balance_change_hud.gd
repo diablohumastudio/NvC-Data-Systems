d22e3e5883359328e1cc4872076134de5bfd401d
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-06 09:29:37 -0500

    game fixed vidually a little bit so everything inside fits

--- screens/game/debug_components/dbug_balance_change_hud/dbug_balance_change_hud.gd
+++ screens/game/debug_components/dbug_balance_change_hud/dbug_balance_change_hud.gd
@@ -1,9 +1,7 @@
-extends HBoxContainer
-
+extends VBoxContainer
 
 func _on_add_50_btn_pressed() -> void:
 	GSS.add_value_to_balance(50)
-
 
 func _on_remove_50_btn_pressed() -> void:
 	GSS.substract_value_from_balance(50)
--- screens/game/debug_components/dbug_canons_killed_hud/dbug_canon_hud.gd
+++ screens/game/debug_components/dbug_canons_killed_hud/dbug_canon_hud.gd
@@ -2,4 +2,4 @@ class_name DbugCanonHud extends VBoxContainer
 
 func _on_canon_killed_btn_pressed() -> void:
 	GSS.canons_alive -= 1
-	%CanonsLeft.text = str(GSS.canons_alive) + " Canons Left"
+	%CanonsLeft.text = str(GSS.canons_alive) + " Left"
