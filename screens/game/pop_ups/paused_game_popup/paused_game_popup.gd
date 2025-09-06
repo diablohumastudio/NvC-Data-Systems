5336531b7fdaa15a16a1b441d1652cb01152aa98
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-06 09:14:36 -0500

    pause popup corrected most

--- screens/game/pop_ups/paused_game_popup/paused_game_popup.gd
+++ screens/game/pop_ups/paused_game_popup/paused_game_popup.gd
@@ -3,14 +3,12 @@ class_name PausedGamePopup extends Control
 signal cancel_input_received
 signal start_key_input_received
 
-const MAIN_MENU = "uid://cflg2ypu25js2"
-const GAME_SCREEN = "uid://0yp607cucppk"
-
-var level_to_replay : LevelData
+@export var level : LevelData
 var _saved_settings = UDS.current_user_data.settings
 
 func _ready():
-	show_popup()
+	if GSS.level: level = GSS.level
+	appear()
 
 func _hide_popup():
 	AudioSystem.post_event(AK.EVENTS.RESET_POPUP_FILTER)
@@ -22,10 +20,27 @@ func _hide_popup():
 	await %AnimationPlayer.animation_finished
 	self.visible = false
 	
-func show_popup():
+func appear():
 	AudioSystem.post_event(AK.EVENTS.SET_POPUP_FILTER)
 	%AnimationPlayer.play("appear")
 
 func _on_resume_game_button_pressed() -> void:
-	print("Sss")
-	pass # Replace with function body.
+	pass
+
+func _on_go_to_main_menu_button_pressed():
+	var main_menu : PackedScene = load(GC.SCREENS_UIDS.MAIN_MENU)
+	
+	AudioSystem.post_event(AK.EVENTS.PLAY_BUTTON_MENU_CLICK2)
+	await get_tree().create_timer(.5).timeout
+	
+	get_tree().paused = false
+	SMS.change_scene(main_menu)
+
+func _on_retry_level_button_pressed() -> void:
+	var game : PackedScene = load(GC.SCREENS_UIDS.GAME)
+	
+	AudioSystem.post_event(AK.EVENTS.PLAY_BUTTON_MENU_CLICK2)
+	await get_tree().create_timer(.5).timeout
+	
+	get_tree().paused = false
+	SMS.change_scene(game,{"level": level})
