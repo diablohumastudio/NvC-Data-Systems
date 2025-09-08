f7957b955a3bfc6fa1432eebc09daae76b90a26c
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-08 16:11:11 -0500

    game_over and pause popups fixed. Working OK.

--- screens/game/pop_ups/game_over_popup/game_over_popup.gd
+++ screens/game/pop_ups/game_over_popup/game_over_popup.gd
@@ -5,13 +5,20 @@ class_name GameOverPopup extends Control
 func _ready() -> void:
 	if GSS.level: level = GSS.level
 	GSS.enemy_reached_last_column.connect(on_gss_enemy_reached_last_column)
-	appear()
 
 func on_gss_enemy_reached_last_column():
-	appear()
+	_show()
 
-func appear():
+func _show():
+	get_tree().paused = true
+	AudioSystem.post_event(AK.EVENTS.SET_POPUP_FILTER)
+	visible = true
 	%AnimationPlayer.play("_appear")
+
+func _hide():
+	get_tree().paused = false
+	AudioSystem.post_event(AK.EVENTS.RESET_POPUP_FILTER)
+	visible = false
 
 func _on_go_to_main_menu_button_pressed():
 	var main_menu : PackedScene = load(GC.SCREENS_UIDS.MAIN_MENU)
--- screens/game/pop_ups/paused_game_popup/paused_game_popup.gd
+++ screens/game/pop_ups/paused_game_popup/paused_game_popup.gd
@@ -1,47 +1,41 @@
 class_name PausedGamePopup extends Control
 
-signal cancel_input_received
-signal start_key_input_received
-
 @export var level : LevelData
 var _saved_settings = UDS.current_user_data.settings
 
+var is_shown: bool
+
 func _ready():
 	if GSS.level: level = GSS.level
-	#appear()
 
-func _hide_popup():
-	AudioSystem.post_event(AK.EVENTS.RESET_POPUP_FILTER)
-	AudioSystem.post_event(AK.EVENTS.PLAY_BUTTON_MENU_CLICK2)
-	%ResumePlayer.play("continue_pressed")
-	await %ResumePlayer.animation_finished
-	get_tree().paused = false
-	%AnimationPlayer.play("dissapear")
-	await %AnimationPlayer.animation_finished
-	self.visible = false
-	
-func appear():
+func _show():
+	get_tree().paused = true
 	AudioSystem.post_event(AK.EVENTS.SET_POPUP_FILTER)
 	visible = true
 	%AnimationPlayer.play("appear")
 
-func _on_resume_game_button_pressed() -> void:
-	pass
-
-func _on_go_to_main_menu_button_pressed():
-	var main_menu : PackedScene = load(GC.SCREENS_UIDS.MAIN_MENU)
-	
-	AudioSystem.post_event(AK.EVENTS.PLAY_BUTTON_MENU_CLICK2)
-	await get_tree().create_timer(.5).timeout
-	
+func _hide():
+	AudioSystem.post_event(AK.EVENTS.RESET_POPUP_FILTER)
 	get_tree().paused = false
-	SMS.change_scene(main_menu)
+	%AnimationPlayer.play("dissapear")
+	await %AnimationPlayer.animation_finished
+	self.visible = false
+
+func _on_resume_game_button_pressed() -> void:
+	_hide()
 
 func _on_retry_level_button_pressed() -> void:
 	var game : PackedScene = load(GC.SCREENS_UIDS.GAME)
-	
-	AudioSystem.post_event(AK.EVENTS.PLAY_BUTTON_MENU_CLICK2)
+
 	await get_tree().create_timer(.5).timeout
-	
+
 	get_tree().paused = false
 	SMS.change_scene(game,{"level": level})
+
+func _on_go_to_main_menu_button_pressed():
+	var main_menu : PackedScene = load(GC.SCREENS_UIDS.MAIN_MENU)
+
+	await get_tree().create_timer(.5).timeout
+
+	get_tree().paused = false
+	SMS.change_scene(main_menu)
