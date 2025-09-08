211050b3a776eee37e00a9e72c106da3b10150e6
diablohumastudioadmin <admin@diablohumastudio.com>
2025-09-08 17:38:14 -0500

    game_won_popup added. Working OK

---
+++ screens/game/pop_ups/game_won_popup/game_won_popup.gd
@@ -0,0 +1,36 @@
+class_name GameWonPopup extends Control
+
+@export var level : LevelData
+
+func _ready():
+	if GSS.level: level = GSS.level
+
+func _show():
+	get_tree().paused = true
+	AudioSystem.post_event(AK.EVENTS.SET_POPUP_FILTER)
+	visible = true
+	%AnimationPlayer.play("_appear")
+
+func _hide():
+	AudioSystem.post_event(AK.EVENTS.RESET_POPUP_FILTER)
+	get_tree().paused = false
+	self.visible = false
+
+func _on_retry_level_button_pressed() -> void:
+	var game : PackedScene = load(GC.SCREENS_UIDS.GAME)
+
+	await get_tree().create_timer(.5).timeout
+
+	get_tree().paused = false
+	SMS.change_scene(game,{"level": level})
+
+func _on_go_to_main_menu_button_pressed():
+	var main_menu : PackedScene = load(GC.SCREENS_UIDS.MAIN_MENU)
+
+	await get_tree().create_timer(.5).timeout
+
+	get_tree().paused = false
+	SMS.change_scene(main_menu)
+
+func _on_continue_button_pressed() -> void:
+	pass # Replace with function body.
