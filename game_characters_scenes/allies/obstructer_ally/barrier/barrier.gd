ed6f054c9474f0cef517bedf2a4fc5c70892574a
zur11 <cibka11@gmail.com>
2025-09-18 17:55:52 -0500

    added AnimatedSprite2D texture updating in Barrier. also implemented basic receive_damage function

--- game_characters_scenes/allies/obstructer_ally/barrier/barrier.gd
+++ game_characters_scenes/allies/obstructer_ally/barrier/barrier.gd
@@ -1,12 +1,47 @@
 class_name Barrier extends CharacterBody2D
 
+## This character has method calls in this animation of AnimationPlayer: "death" (Node.queue_free())
+
+@export var initial_hp : float
+var hp : float
 var dying : bool = false
 
 func _on_test_dying_btn_pressed() -> void:
 	_die()
 
+func _ready() -> void:
+	hp = initial_hp
+	
+
+func receive_damage(damage_points:float) -> void:
+	hp -= damage_points
+	
+	_check_dying_conditions()
+	_update_base_texture()
+	
+func _check_dying_conditions() -> void:
+	if hp <= 0:
+		_die()
+	
+func _update_base_texture() -> void:
+	if hp <= initial_hp * 0.25:
+		%Base.play("damaged_3")
+	elif hp <= initial_hp * 0.5:
+		%Base.play("damaged_2")
+	elif hp <= initial_hp * 0.75:
+		print("result: ", initial_hp * 0.75)
+		%Base.play("damaged_1")
+	#else:
+		#%Base.play("default")
+
+func _on_anim_tree_animation_finished(animation_name:String) -> void:
+	if animation_name == "receive_damage":
+		print("receive_damage animation finished")
+
 func _die() -> void:
 	dying = true
 
-func _self_erase() -> void:
-	queue_free()
+func _on_test_damage_button_pressed() -> void:
+	receive_damage(2.0)
+	%AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	print("hp: ", hp)
