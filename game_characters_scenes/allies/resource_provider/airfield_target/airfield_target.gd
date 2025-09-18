092490d9cb038de4c7db9af1c9a8cd453e41f12c
zur11 <cibka11@gmail.com>
2025-09-18 17:03:47 -0500

    some improvements in airfield_target

--- game_characters_scenes/allies/resource_provider/airfield_target/airfield_target.gd
+++ game_characters_scenes/allies/resource_provider/airfield_target/airfield_target.gd
@@ -1,22 +1,20 @@
 class_name AirfieldTarget extends CharacterBody2D
 
+## This character has method calls in these animations of AnimationPlayer: "give_coin" (_on_give_coin_finished) and "death" (Node.queue_free())
+
 var giving_coin : bool = false
 
 @onready var state_machine_playback : AnimationNodeStateMachinePlayback = %AnimationTree.get("parameters/StateMachine/playback")
 
 func _ready() -> void:
 	%GiveCoinTimer.timeout.connect(_on_give_coin_timer_timeout)
-	%AnimationTree.animation_finished.connect(_on_anim_tree_animation_finished)
 
 func _on_give_coin_timer_timeout() -> void:
 	giving_coin = true
 
-func _on_anim_tree_animation_finished(animation_name:String) -> void:
-	if animation_name == "give_coin":
-		giving_coin = false
-		%GiveCoinTimer.start()
-	elif animation_name == "death":
-		queue_free()
+func _on_give_coin_finished() -> void:
+	giving_coin = false
+	%GiveCoinTimer.start()
 
 func _die() -> void:
 	state_machine_playback.travel("death")
