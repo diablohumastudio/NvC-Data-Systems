0da527a6699145a6f6865dc6ff7a7a60211de7fb
zur11 <cibka11@gmail.com>
2025-09-19 23:22:51 -0500

    improved and ordered all resource provider allies animation trees. AirfieldTarget and WoodenChest use the shortest version and IronChest uses a little larger as it uses a property to transition to give_coin state

--- game_characters_scenes/allies/resource_provider/airfield_target/airfield_target.gd
+++ game_characters_scenes/allies/resource_provider/airfield_target/airfield_target.gd
@@ -1,19 +1,17 @@
 class_name AirfieldTarget extends CharacterBody2D
 
-## This character has a method call in this animation of AnimationPlayer: 
-## "death" (Node.queue_free())
-
 @onready var state_machine_playback : AnimationNodeStateMachinePlayback = %AnimationTree.get("parameters/StateMachine/playback")
 
-func _on_give_coin_timer_timeout() -> void:
-	state_machine_playback.travel("give_coin")
+func _on_test_dying_btn_pressed() -> void:
+	_die()
 
 ## This function is called from animation: "give_coin"
 func _on_give_coin_finished() -> void:
 	%GiveCoinTimer.start()
+	
+func _on_give_coin_timer_timeout() -> void:
+	state_machine_playback.travel("give_coin")
 
+## At the end of "death" animation there is a call to method: Node.queue_free()
 func _die() -> void:
 	state_machine_playback.travel("death")
-
-func _on_test_dying_btn_pressed() -> void:
-	_die()
--- game_characters_scenes/allies/resource_provider/wooden_chest/wooden_chest.gd
+++ game_characters_scenes/allies/resource_provider/wooden_chest/wooden_chest.gd
@@ -1,34 +1,17 @@
 class_name WoodenChest extends CharacterBody2D
 
-## This character has method calls in these animations of AnimationPlayer: 
-## "idle" (_on_idle_animation_finished()) "give_coin" (_on_give_coin_finished()) and "death" (Node.queue_free())
-
 @onready var state_machine_playback : AnimationNodeStateMachinePlayback = %AnimationTree.get("parameters/StateMachine/playback")
-
-func _toggle_idle_loop_mode() -> void:
-	var idle_animation : Animation = %AnimationPlayer.get_animation("idle")
-	
-	if idle_animation.loop_mode == Animation.LOOP_LINEAR:
-		idle_animation.loop_mode = Animation.LOOP_NONE
-	elif idle_animation.loop_mode == Animation.LOOP_NONE:
-		idle_animation.loop_mode = Animation.LOOP_LINEAR
-
-func _on_idle_animation_finished() -> void:
-	var idle_animation : Animation = %AnimationPlayer.get_animation("idle")
-	
-	if idle_animation.loop_mode == Animation.LOOP_NONE:
-		state_machine_playback.travel("give_coin")
-
-func _on_give_coin_finished() -> void:
-	_toggle_idle_loop_mode()
-	state_machine_playback.travel("idle")
-	%GiveCoinTimer.start()
-
-func _on_give_coin_timer_timeout() -> void:
-	_toggle_idle_loop_mode()
-
-func _die() -> void:
-	state_machine_playback.travel("death")
 
 func _on_test_dying_btn_pressed() -> void:
 	_die()
+
+## This function is called from animation: "give_coin"
+func _on_give_coin_finished() -> void:
+	%GiveCoinTimer.start()
+	
+func _on_give_coin_timer_timeout() -> void:
+	state_machine_playback.travel("give_coin")
+
+## At the end of "death" animation there is a call to method: Node.queue_free()
+func _die() -> void:
+	state_machine_playback.travel("death")
