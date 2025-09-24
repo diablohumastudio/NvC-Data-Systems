f0204fb20aaece311e150e46196ad2d4124ecaa8
zur11 <cibka11@gmail.com>
2025-09-24 17:40:47 -0500

    changed BayonetSoldier's system to work with check_transitions animation/state. It has some small fixing pending to match check_transitions and idle keyframes

--- game_characters_scenes/allies/shoter_ally/bayonet_soldier/bayonet_soldier.gd
+++ game_characters_scenes/allies/shoter_ally/bayonet_soldier/bayonet_soldier.gd
@@ -1,11 +1,17 @@
 class_name BayonetSoldier extends CharacterBody2D
 
-var cooldown_idle_cycle_complete : bool = false
-var short_opponent_detected : bool
+## This character has a method call in this animation of AnimationPlayer: 
+## "death" (Node.queue_free())
+
+const MAX_WAIT_CYCLES : int = 2
+
+var wait_cycles_counter : int = 0
+var are_wait_cycles_finished : bool
 var long_opponent_detected : bool
+var short_opponent_detected : bool
+var needs_reload : bool
 
 @onready var state_machine_playback : AnimationNodeStateMachinePlayback = %AnimationTree.get("parameters/StateMachine/playback")
-@onready var idle_animation : Animation = %AnimationPlayer.get_animation("idle")
 
 #region For testing Only. Just creates an object where mouse click
 const TEST_BODY_PKSC: PackedScene = preload("uid://didykuwbgj0mj")
@@ -19,37 +25,29 @@ func _unhandled_input(event: InputEvent) -> void:
 		new_test_body.global_position = event.position
 #endregion
 
-func _ready() -> void:
-	%AnimationTree.animation_finished.connect(_on_anim_tree_animation_finished)
-
-func _check_for_opponents() -> void:
-	short_opponent_detected = %ShortOpponentsArea.has_overlapping_bodies()
+## This function is called from animation: "check_transitions"
+func _check_transitions() -> void:
 	long_opponent_detected = %LongOpponentsArea.has_overlapping_bodies()
-	
-	if short_opponent_detected or long_opponent_detected:
-		idle_animation.loop_mode = Animation.LOOP_NONE
+	short_opponent_detected = %ShortOpponentsArea.has_overlapping_bodies()
+	are_wait_cycles_finished = wait_cycles_counter == MAX_WAIT_CYCLES
+	needs_reload = false
 
-func _on_anim_tree_animation_finished(animation_name:String) -> void:
-	print("animation finished: ", animation_name)
-	if animation_name == "idle":
-		if !cooldown_idle_cycle_complete:
-			state_machine_playback.start("idle")
-			cooldown_idle_cycle_complete = true
-		else:
-			cooldown_idle_cycle_complete = false
-			if short_opponent_detected:
-				state_machine_playback.travel("stab")
-			elif long_opponent_detected and !short_opponent_detected:
-				state_machine_playback.travel("shoot")
-			elif !short_opponent_detected and !long_opponent_detected:
-				idle_animation.loop_mode = Animation.LOOP_LINEAR
-				state_machine_playback.start("idle")
+	if !(short_opponent_detected or long_opponent_detected):
+		return # No enemies detected so no need to use idle_cycle_counter
+	_update_wait_cycle_counter()
+
+func _update_wait_cycle_counter() -> void:
+	if wait_cycles_counter == MAX_WAIT_CYCLES:
+		wait_cycles_counter = 0 # If idle cycles finished, reset idle_cycles_counter and return
+		return
+	wait_cycles_counter += 1
+
+## This function is called from animation: "shoot"
+func _on_shoot_animation_finishing() -> void:
+	needs_reload = true
 
 func _on_test_dying_btn_pressed() -> void:
 	_die()
 
 func _die() -> void:
 	state_machine_playback.travel("death")
-
-func _self_erase() -> void:
-	queue_free()
