d3c4fdaece1a05ad26ab537b69e508ff495aac8d
zur11 <cibka11@gmail.com>
2025-09-25 11:49:27 -0500

    created a test ally_level upgrade to implement level upgrade changes

---
+++ tests/ally_level_upgrade/test_ally_level_upgrade.gd
@@ -0,0 +1,6 @@
+class_name TestAllyLevelUpgrade extends Resource
+
+var shooting_power_increment : float
+var shooting_speed_increment : float
+var shooting_range_increment : int
+var hp_increment : float 
