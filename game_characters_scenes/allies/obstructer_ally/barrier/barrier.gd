86feb95b718e93ac7d48ab8ab6f58c497c1e44a4
zur11 <cibka11@gmail.com>
2025-09-25 06:52:51 -0500

    improved Barrier's receive_damage func adding 2 new variables, dying and receiving_damage to prevent receive_damage animation replaying

--- game_characters_scenes/allies/obstructer_ally/barrier/barrier.gd
+++ game_characters_scenes/allies/obstructer_ally/barrier/barrier.gd
@@ -5,16 +5,27 @@ class_name Barrier extends CharacterBody2D
 
 @export var initial_hp : float
 var hp : float
-var dying : bool = false
+var dying : bool
+var receiving_damage : bool
 
 func _on_test_dying_btn_pressed() -> void:
 	_die()
 
+func _on_test_damage_button_pressed() -> void:
+	receive_damage(2.0)
+	print("hp: ", hp)
+	
 func _ready() -> void:
 	hp = initial_hp
 	%AnimationTree.animation_finished.connect(_on_anim_tree_animation_finished)
 	
 func receive_damage(damage_points:float) -> void:
+	if dying:
+		return
+	if receiving_damage:
+		hp -= damage_points
+		return
+	receiving_damage = true
 	hp -= damage_points
 	%AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 	
@@ -34,10 +45,7 @@ func _on_anim_tree_animation_finished(animation_name:String) -> void:
 	if animation_name == "receive_damage":
 		_check_dying_conditions()
 		_update_base_texture()
+		receiving_damage = false
 
 func _die() -> void:
 	dying = true
-
-func _on_test_damage_button_pressed() -> void:
-	receive_damage(2.0)
-	print("hp: ", hp)
