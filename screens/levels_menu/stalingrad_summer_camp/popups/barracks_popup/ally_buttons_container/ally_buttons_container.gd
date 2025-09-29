dd9de107e06f0f366a6baad482e989858ea7b63a
zur11 <cibka11@gmail.com>
2025-09-29 10:19:29 -0500

    created AllyPreview to display selected ally scene in BarracksPopup

--- screens/levels_menu/stalingrad_summer_camp/popups/barracks_popup/ally_buttons_container/ally_buttons_container.gd
+++ screens/levels_menu/stalingrad_summer_camp/popups/barracks_popup/ally_buttons_container/ally_buttons_container.gd
@@ -4,6 +4,8 @@ signal ally_btn_pressed(ally_btn:AllyBtn)
 
 const _ALLY_BTN_SCENE_UID : String = "uid://cj1lvhqy5v3dr"
 
+var selected_ally_btn : AllyBtn
+
 func populate_container(allies:Array[AllyData]) -> void:
 	for child in %ButtonsContainer.get_children():
 		child.queue_free()
@@ -13,6 +15,10 @@ func populate_container(allies:Array[AllyData]) -> void:
 		new_ally_btn.ally = ally
 		%ButtonsContainer.add_child(new_ally_btn)
 		new_ally_btn.pressed.connect(_on_ally_btn_pressed.bind(new_ally_btn))
+	
+	selected_ally_btn = %ButtonsContainer.get_child(0)
 
 func _on_ally_btn_pressed(ally_btn:AllyBtn) -> void:
+	selected_ally_btn.button_pressed = false
 	ally_btn_pressed.emit(ally_btn)
+	selected_ally_btn = ally_btn
