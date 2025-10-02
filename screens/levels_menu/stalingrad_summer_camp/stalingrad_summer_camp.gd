23e7ac25c84ab7c41cb300ae61d86f8cc094fdc3
zur11 <cibka11@gmail.com>
2025-10-02 05:44:24 -0500

    removed unused button in SummerCamp

--- screens/levels_menu/stalingrad_summer_camp/stalingrad_summer_camp.gd
+++ screens/levels_menu/stalingrad_summer_camp/stalingrad_summer_camp.gd
@@ -5,9 +5,6 @@ class_name StalingradSummerCamp extends Control
 func _on_barracks_pressed() -> void:
 	barracks_popup.show_popup()
 
-func _on_texture_button_2_pressed() -> void:
-	barracks_popup.hide_popup()
-
 func _on_go_back_button_pressed() -> void:
 	var stalingrad_summer_levels_menu : PackedScene = SLS.get_scene(GC.SCREENS_UIDS.STALINGRAD_SUMMER_LEVELS_MENU)
 	SMS.change_scene(stalingrad_summer_levels_menu, {"entering_from_stalingrad_summer_camp": true})
